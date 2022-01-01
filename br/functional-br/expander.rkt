4bb47d8fbe2825b4faca96fb1a0497da8d87130f
Lucy Hao <hao.lucyy@gmail.com>
2022-01-01 10:54:01 -0700

    done functional bf

--- br/functional-br/expander.rkt
+++ br/functional-br/expander.rkt
@@ -5,34 +5,58 @@
      PARSE-TREE))
 (provide (rename-out [bf-module-begin #%module-begin]))
 
+(define (fold-funcs apl bf-funcs)
+  (for/fold ((current-apl apl))
+            ((bf-func (in-list bf-funcs)))
+    (apply bf-func current-apl)))
+
 (define-macro (bf-program OP-OR-LOOP-ARG ...)
-  #'(void OP-OR-LOOP-ARG ...))
+  #'(begin
+      (define first-apl (list (make-vector 30000 0) 0))
+      (void (fold-funcs first-apl (list OP-OR-LOOP-ARG ...)))))
 (provide bf-program)
 
 (define-macro (bf-loop "[" OP-OR-LOOP-ARG ... "]")
-  #'(until (zero? (current-byte))
-           OP-OR-LOOP-ARG ...))
+  #'(lambda (arr ptr)
+      (for/fold ([current-apl (list arr ptr)])
+                ([i (in-naturals)]
+                 #:break (zero? (apply current-byte
+                                       current-apl)))
+        (fold-funcs current-apl (list OP-OR-LOOP-ARG ...)))))
 (provide bf-loop)
 
 (define-macro-cases bf-op
-  [(bf-op ">") #'(gt)]
-  [(bf-op "<") #'(lt)]
-  [(bf-op "+") #'(plus)]
-  [(bf-op "-") #'(minus)]
-  [(bf-op ".") #'(period)]
-  [(bf-op ",") #'(comma)])
+  [(bf-op ">") #'gt]
+  [(bf-op "<") #'lt]
+  [(bf-op "+") #'plus]
+  [(bf-op "-") #'minus]
+  [(bf-op ".") #'period]
+  [(bf-op ",") #'comma])
 (provide bf-op)
 
-(define arr (make-vector 30000 0))
-(define ptr 0)
+(define (current-byte arr ptr) (vector-ref arr ptr))
 
-(define (current-byte) (vector-ref arr ptr))
+(define (set-current-byte arr ptr val)
+  (define new-arr (vector-copy arr))
+  (vector-set! new-arr ptr val)
+  new-arr)
 
-(define (set-current-byte! val) (vector-set! arr ptr val))
+(define (gt arr ptr) (list arr (add1 ptr)))
+(define (lt arr ptr) (list arr (sub1 ptr)))
 
-(define (gt) (set! ptr (add1 ptr)))
-(define (lt) (set! ptr (sub1 ptr)))
-(define (plus) (set-current-byte! (add1 (current-byte))))
-(define (minus) (set-current-byte! (sub1 (current-byte))))
-(define (period) (write-byte (current-byte)))
-(define (comma) (set-current-byte! (read-byte)))
+(define (plus arr ptr)
+  (list
+   (set-current-byte arr ptr (add1 (current-byte arr ptr)))
+   ptr))
+
+(define (minus arr ptr)
+  (list
+   (set-current-byte arr ptr (sub1 (current-byte arr ptr)))
+   ptr))
+
+(define (period arr ptr)
+  (write-byte (current-byte arr ptr))
+  (list arr ptr))
+
+(define (comma arr ptr)
+  (list (set-current-byte arr ptr (read-byte)) ptr))
